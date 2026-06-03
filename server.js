c799f785bf88adce3e84a7322dbd623875e32870
amalhamid111-crypto <amalhamid111@gmail.com>
2026-06-03 11:04:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
